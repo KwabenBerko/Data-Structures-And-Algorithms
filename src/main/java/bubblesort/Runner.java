eb329dbd586afa8847d7cf5a55b358247196555e
Kwabena Berko <kwabenaberko95@gmail.com>
2020-04-05 14:26:23 +0000

    Minor Update

--- src/main/java/bubblesort/Runner.java
+++ src/main/java/bubblesort/Runner.java
@@ -2,7 +2,7 @@ package bubblesort;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class Runner {
 
     public static void main(String[] args) {
         int[] numbers = new int[]{15, 5, 24, 8, 1, 3, 16, 10, 20};
