4df5725e2049c1e0e5fdb92c24aaed0185740515
Kwabena Berko <kwabenaberko95@gmail.com>
2020-04-05 12:09:47 +0000

    Selection Sort

---
+++ src/main/java/selectionsort/Runner.java
@@ -0,0 +1,29 @@
+package selectionsort;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main (String[] args){
+        int[] numbers = new int[]{15, 5, 24, 8, 1, 3, 16, 10, 20};
+        selectionSort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static void selectionSort(int[] array){
+
+        //5 | 9, 7, 8
+
+        for(int i = 0; i < array.length; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < array.length; j++){
+                if(array[j] < array[minIndex]){
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+}
